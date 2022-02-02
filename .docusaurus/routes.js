67bfea8b4b797fa126c50f30e60fe10ad5bc3f99
sparsh <sparsh.cloud@gmail.com>
2022-02-02 17:35:41 +0000

    commit

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -4,41 +4,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug','3d6'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config','914'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content','c28'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry','0da'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes','244'),
-    exact: true
-  },
-  {
     path: '/blog',
     component: ComponentCreator('/blog','b52'),
     exact: true
@@ -345,7 +310,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','218'),
+    component: ComponentCreator('/docs','596'),
     routes: [
       {
         path: '/docs/concept-basics/challenges',
@@ -518,12 +483,6 @@ export default [
       {
         path: '/docs/concept-extras/mlops',
         component: ComponentCreator('/docs/concept-extras/mlops','760'),
-        exact: true,
-        'sidebar': "tutorialSidebar"
-      },
-      {
-        path: '/docs/concept-extras/nlp/',
-        component: ComponentCreator('/docs/concept-extras/nlp/','cba'),
         exact: true,
         'sidebar': "tutorialSidebar"
       },
