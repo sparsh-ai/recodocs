984271b2e1cece0f5b0c19ae4a5e44882fc03101
sparsh <sparsh.cloud@gmail.com>
2022-02-26 09:58:21 +0000

    commit

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -4,6 +4,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug','3d6'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config','914'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content','c28'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry','0da'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes','244'),
+    exact: true
+  },
+  {
     path: '/blog',
     component: ComponentCreator('/blog','b52'),
     exact: true
@@ -310,7 +345,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs','631'),
+    component: ComponentCreator('/docs','d10'),
     routes: [
       {
         path: '/docs/concept-basics/',
@@ -1357,8 +1392,20 @@ export default [
         'sidebar': "tutorialSidebar"
       },
       {
+        path: '/docs/tutorials/negative-implicit-feedback-rec',
+        component: ComponentCreator('/docs/tutorials/negative-implicit-feedback-rec','b34'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
         path: '/docs/tutorials/pdf-to-wordcloud-mail',
         component: ComponentCreator('/docs/tutorials/pdf-to-wordcloud-mail','6a5'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/tutorials/vector-search',
+        component: ComponentCreator('/docs/tutorials/vector-search','1ab'),
         exact: true,
         'sidebar': "tutorialSidebar"
       }
